d1293ba98edd92a253976804de83d7e6a0f75e1d
Hendri <hendri.gnw@gmail.com>
2016-07-14 15:46:16 +0700

    adding function formatIndoDate and rupiah

--- helpers/StringHelper.php
+++ helpers/StringHelper.php
@@ -22,4 +22,28 @@ class StringHelper extends BaseStringHelper
 
         return $date->format($format);
     }
+	
+	/**
+	 * the date format into indonesian
+	 * 
+	 * @param type $date date or datetime
+	 * @param type $format eg. (%Y-%m-%d) 
+	 * @see type $format http://php.net/manual/en/function.strftime.php
+	 * @return type
+	 */
+	public static function formatIndoDate($date, $format)
+	{
+		setLocale(LC_ALL, 'id_ID', 'ind', 'indonesia');
+		return strftime($format, strtotime($date));
+	}
+	
+	/**
+     * Format number to rupiah
+     * @param float $value
+     * @return string
+     */
+    public static function rupiah($value)
+    {
+        return number_format($value, 0, ',', '.');
+    }
 }
