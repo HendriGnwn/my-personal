8f3f17691e35273086d0b97e7dfc223e2c8074f5
Hendri <hendri.gunawan@computesta.com>
2016-07-15 11:27:30 +0700

    adding params mobileDomain, defaultWebPictureUrl and defaultWebPicture

--- config/params.php
+++ config/params.php
@@ -3,6 +3,9 @@
 return [
     'adminEmail' => 'admin@example.com',
     'mainDomain' => 'http://www.hendrigunawan.com',
+	'mobileDomain' => 'http://m.hendrigunawan.com',
+	'defaultWebPictureUrl' => '@web/data/images/picture-main.jpg',
+	'defaultWebPicture' => 'picture-main.jpg',
     'jsBlock' => 0,
     'modalBlock' => 0,
 	'subjectMailto' => 'Hello%20Hendri%20Gunawan'
